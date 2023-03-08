209140840cfecfb77cffc32489754e363579a033
Saiesh <saiesh_naik@persistent.com>
2023-03-08 19:50:43 +0530

    main:Footer.js file updated, chaosai16 link update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import {AiFillGithub} from 'react-icons/ai'
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 function Footer() {
   return (
-    <div className='flex justify-center text-center items-center bg-blue-400 p-3'>
-      <a className='flex  text-black' href='https://github.com/chaosai16'target="_blank">@chaosai16</a>
-      <AiFillGithub size={20} className='mx-2'/>
+    <div className="flex justify-center text-center items-center bg-blue-400 p-3">
+      <a className="flex  text-black" href="https://github.com/chaosai16">
+        @chaosai16
+      </a>
+      <AiFillGithub size={20} className="mx-2" />
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
